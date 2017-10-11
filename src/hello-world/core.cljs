3137d92fb3bbbcd8bf33d20683578e44703c3a73
Felix Krull <f_krull@gmx.de>
2017-10-11 22:47:46 +0200

    Register and run command.

--- src/hello-world/core.cljs
+++ src/hello-world/core.cljs
@@ -1,9 +1,15 @@
 (ns hello-world.core
     (:require [cljs.nodejs :as nodejs]))
+(def vscode (js/require "vscode"))
+
+(defn hello-world []
+    (.showInformationMessage (.-window vscode) "Hello ClojureScript!"))
 
 (defn ^:export activate [context]
     (nodejs/enable-util-print!)
-    (println "Hello from your ClojureScript extension!"))
+    (println "Hello from your ClojureScript extension!")
+    (.push (.-subscriptions context)
+        (.registerCommand (.-commands vscode) "hellocljs.helloWorld" hello-world)))
 
 (defn ^:export deactivate []
     (println "Goodbye from your ClojureScript extension."))
